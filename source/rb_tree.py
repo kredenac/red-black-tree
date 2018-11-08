d308e17ab3fcc7451187932d58a43ba4067e775e
kredenac <zacementirano@gmail.com>
2018-11-08 02:07:48 +0100

    Inserting works.

--- source/rb_tree.py
+++ source/rb_tree.py
@@ -10,17 +10,16 @@ class Node():
     def insert(self, newVal):
         lastNode = None
         currNode = self
-        if newVal == self.val:
-            assert "TODO: decide whether to ignore or not"
         # go all the way to the leaf
         while currNode is not None:
             lastNode = currNode
-            if newVal < self.val:
-                currNode = self.left
+            if newVal < currNode.val:
+                currNode = currNode.left
             else:
-                currNode = self.right
+                currNode = currNode.right
+
         newNode = Node(newVal, par=lastNode)
-        if newVal < lastNode:
+        if newVal < lastNode.val:
             lastNode.left = newNode
         else:
             lastNode.right = newNode
@@ -48,6 +47,11 @@ class Node():
         isUncleRed = False if uncle is None else uncle.isRed
         if isUncleRed:
             # case 1: z.uncle = red => recolor parent, grandp, uncle
+            grampa = self.grandparent()
+            self.par.isRed = False
+            grampa.isRed = True
+            uncle.isRed = False
+            grampa.insert_repair()
             return
 
         # else, uncle is black
@@ -102,28 +106,40 @@ class Node():
     # become left child of right son
     def leftRotate(self):
         oldPar = self.par
-        wasLeftSon = oldPar.left == self
+        wasRoot = self.isRoot()
         rightSon = self.right
         rightSonOldLeft = rightSon.left
         rightSon.left = self
-        if wasLeftSon:
-            oldPar.left = rightSon
-        else:
-            oldPar.right = rightSon
+        if not wasRoot:
+            wasLeftSon = oldPar.left == self
+            if wasLeftSon:
+                oldPar.left = rightSon
+            else:
+                oldPar.right = rightSon
+        if rightSonOldLeft is not None:
+            rightSonOldLeft.par = self
         self.right = rightSonOldLeft
+        self.par = rightSon
+        rightSon.par = oldPar
     
     # become right child of left son
     def rightRotate(self):
         oldPar = self.par
-        wasLeftSon = oldPar.left == self
+        wasRoot = self.isRoot()
         leftSon = self.left
         leftSonOldRight = leftSon.right
         leftSon.right = self
-        if wasLeftSon:
-            oldPar.left = leftSon
-        else:
-            oldPar.right = leftSon
+        wasLeftSon = oldPar.left == self
+        if not wasRoot:
+            if wasLeftSon:
+                oldPar.left = leftSon
+            else:
+                oldPar.right = leftSon
+        if leftSonOldRight is not None:
+            leftSonOldRight.par = self
         self.left = leftSonOldRight
+        self.par = leftSon
+        leftSon.par = oldPar
 
     def parent(self):
         return self.par
@@ -147,19 +163,20 @@ class Node():
             return grand.left
     
     def __str__(self):
-        return str(self.val) + "R" if self.isRed else "B"
+        return str(self.val) + ("R" if self.isRed else "B")
     
-    def __print(self, level=0):
+    def print(self, level=0):
         print(f"{'__'*level}{self}")
         if self.right is not None:
-            self.right.__print(level+1)
+            self.right.print(level+1)
+            print()
         if self.left is not None:
-            self.left.__print(level+1)
+            self.left.print(level+1)
         
 
 class RBTree():
     def __init__(self, root=None):
-        self.root = None
+        self.root = root
     def insert(self, newVal):
         if self.root is None:
             self.root = Node(newVal, par=None, isRed=False)
@@ -169,18 +186,28 @@ class RBTree():
     def remove(self, newVal):
         self.root = self.root.remove(newVal)
         
-    def printTree(self, level=0):
+    def print(self, level=0):
         if self.root is None:
             print("Empty tree.")
             return
-        self.root.__print()
+        self.root.print()
         
-        
-        
-
-
 def main():
-    print("Begin")
+    print("Hi")
+    tree = RBTree()
+    
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+    tree.insert(5)
+    tree.insert(1)
+    tree.insert(1)
+
+
+    tree.print()
+
+    print("Bye")
 
 if __name__ == "__main__":
     main()
